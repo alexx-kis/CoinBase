8f6b3044b23fb03956414d7874530aef13268890
Alexander <fenaethylaamiinloever@gmail.com>
2023-08-05 19:14:41 +0300

    animated

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ burger.addEventListener('click', function () {
 	menu.classList.toggle('header__menu--open');
 	overlay.classList.toggle('overlay--show');
 	sellmenu.classList.toggle('header__sell-menu--open');
-	
+
 });
 
 
@@ -32,4 +32,51 @@ for (let title of titles) {
 			this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + "px"
 		}
 	});
+}
+
+/*==================================== ANIMATION ====================================*/
+
+window.onload = function () {
+	let loadings = document.querySelectorAll('.loading');
+	let header = document.querySelector('.header');
+	header.style.opacity = 1;
+	
+
+	for (let loading of loadings) {
+		loading.classList.add('loaded');
+	}
+};
+
+let animItems = document.querySelectorAll('.anim-item');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let i = 0; i < animItems.length; i++) {
+			let animItem = animItems[i];
+			let animItemHeight = animItem.offsetHeight;
+			let animItemOffset = offset(animItem).top;
+			let animStart = 4
+			let animItemPoint = window.innerHeight - (animItemHeight / animStart);
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - (window.innerHeight / animStart);
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && (pageYOffset < (animItemOffset + animItemHeight))) {
+				animItem.classList.add('anim-item--active');
+			} else {
+				if (!animItem.classList.contains('anim-no-hide')) {
+					animItem.classList.remove('anim-item--active');
+				}
+			}
+
+			function offset(elem) {
+				let rect = elem.getBoundingClientRect(),
+					scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+					scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+				return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+			}
+		}
+	}
 }
